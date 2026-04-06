a9fa05eeab25774b4f6a3173730fd9e8c98e681c
wldnz <wildanofficial32@gmail.com>
2026-04-07 01:07:18 +0700

    ref: change ContactSupport to ContactSupportMail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -111,7 +111,7 @@ class ContactController extends Controller
                 'message' => $request->message,
             ];
 
-            Mail::to($contact->email)->send(new \App\Mail\ContactSupport($mailData));
+            Mail::to($contact->email)->send(new \App\Mail\ContactSupportMail($mailData));
 
             $response = $this->responseData->create(
                 'Pesan berhasil dikirim! Tim Support akan segera menghubungi Anda.',
