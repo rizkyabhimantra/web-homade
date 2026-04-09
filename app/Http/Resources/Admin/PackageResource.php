82921614e80f99dd247f49057f3957fcd2ef2321
wldnz <wildanofficial32@gmail.com>
2026-04-09 19:11:07 +0700

    fix: fix error PackageResource On MenuController (Admin)

---
+++ app/Http/Resources/Admin/PackageResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PackageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'minimum_order' => $this->minimum_order,
+            'url' => $this->image_url,
+        ];
+    }
+}
